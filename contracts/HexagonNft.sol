749263ead9068aa9e9117fa1b22356c7567cee1b
Adebayo Temitope <adecancode@gmail.com>
2022-07-16 16:56:33 +0100

    update

--- contracts/HexagonNft.sol
+++ contracts/HexagonNft.sol
@@ -10,10 +10,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract HexagonNft is ERC721URIStorage, Ownable, ReentrancyGuard {
+    // token counter
     using Counters for Counters.Counter;
     Counters.Counter private _tokenId;
 
-    address public publicContractAddress;
+    // token properties
     uint256 public mintPrice;
     uint256 public wlPrice;
     uint256 public maxSupply;
@@ -21,8 +22,29 @@ contract HexagonNft is ERC721URIStorage, Ownable, ReentrancyGuard {
     uint256 public maxPerWalletWL;
     uint256 public maxPerWalletMint;
 
+    // Enable Minting
     bool public mintingEnabled;
     bool public wlMintingEnabled;
 
-    constructor() ERC721("HexagonNFT", "HXNFT") {}
+    constructor() ERC721("HexagonNFT", "HXNFT") {
+        mintPrice = 0.4 ether;
+        wlPrice = 0.1 ether;
+        maxSupply = 20;
+        totalSupply = 0;
+        maxPerWalletMint = 4;
+        maxPerWalletWL = 2;
+    }
+
+    function enableMinting(bool enabled) public onlyOwner {
+        mintingEnabled = enabled;
+    }
+
+    function enableWhiteList(bool enabled) public onlyOwner {
+        wlMintingEnabled = enabled;
+    }
+
+    function withdraw() public onlyOwner nonReentrant {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Withdrawal failed");
+    }
 }
